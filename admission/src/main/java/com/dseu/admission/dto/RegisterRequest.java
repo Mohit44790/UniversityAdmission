2617b4a433eafc6102c77c9ee6a12c6ab0d4cb23
mohit <mohit44790@gmail.com>
2026-01-11 13:05:28 +0530

    dto request

--- admission/src/main/java/com/dseu/admission/dto/RegisterRequest.java
+++ admission/src/main/java/com/dseu/admission/dto/RegisterRequest.java
@@ -1,4 +1,13 @@
 package com.dseu.admission.dto;
 
+
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String fullName;
+    private String email;
+    private String mobile;
+    private String password;
+    private String confirmPassword;
 }
--- admission/src/main/java/com/dseu/admission/entity/OtpVerification.java
+++ admission/src/main/java/com/dseu/admission/entity/OtpVerification.java
@@ -1,4 +1,20 @@
 package com.dseu.admission.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class OtpVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String otp;
+    private LocalDateTime expiryTime;
 }
