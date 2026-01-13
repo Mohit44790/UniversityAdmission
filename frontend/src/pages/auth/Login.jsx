68f9973c471767e585f2bcc0a6110f273811e6a0
mohit <mohit44790@gmail.com>
2026-01-13 21:58:32 +0530

    routes

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../../redux/slices/authSlice';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {loading ,error} = useSelector((state) => state.auth);
+
+  const [formData, setFormData] = useState({
+    email:"",
+    password:""
+  })
+
+
+  const handleChange = (e) =>{
+  setFormData({...formData, [e.target.name]:e.target.value});
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await dispatch(loginUser(formData));
+
+    navigate('/dashboard');
+  }
   return (
-    <div>
+    <div className=' flex items-center justify-center bg-linear-to-br from-[#C7C5F4] via-[#9F9BD8] to-[#776BCC] '>
       <div className='relative z-10 h-full flex flex-col justify-center px-10'>
-        <h2>Admission Portal</h2>
-        <form>
+        <h2 className='text-3xl font-bold text-center mb-8 tracking-wide'>Admission Portal</h2>
+        <form onSubmit={handleSubmit} className='space-y-7'>
+          <div className='relative group'>
+            <i className='fas fa-user absolute top-3 left-1 transition' />
+          </div>
 
         </form>
       </div>
--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -28,13 +28,13 @@ const Signup = () => {
     }
   }
   return (
-     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#C7C5F4] via-[#9F9BD8] to-[#776BCC]">
-      <div className="relative w-[500px] h-[580px] bg-gradient-to-br from-[#5D54A4] to-[#7C78B8] shadow-[0_20px_60px_rgba(0,0,0,0.35)] overflow-hidden rounded-3xl">
+     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-[#C7C5F4] via-[#9F9BD8] to-[#776BCC]">
+      <div className="relative w-[500px] h-[580px] bg-linear-to-br from-[#5D54A4] to-[#7C78B8] shadow-[0_20px_60px_rgba(0,0,0,0.35)] overflow-hidden rounded-3xl">
 
         {/* Background Shapes */}
         <span className="absolute w-[520px] h-[520px] bg-white/90 top-[-60px] right-[120px] rotate-45 rounded-tr-[72px]" />
         <span className="absolute w-[220px] h-[220px] bg-[#6C63AC] top-[-180px] right-[-40px] rotate-45 rounded-3xl" />
-        <span className="absolute w-[200px] h-[560px] bg-gradient-to-t from-[#5D54A4] to-[#6A679E] top-[-20px] right-[-20px] rotate-45 rounded-3xl" />
+        <span className="absolute w-[200px] h-[560px] bg-linear-to-t from-[#5D54A4] to-[#6A679E] top-[-20px] right-[-20px] rotate-45 rounded-3xl" />
         <span className="absolute w-[220px] h-[420px] bg-[#7E7BB9] top-[460px] right-[40px] rotate-45 rounded-[80px]" />
 
         {/* Content */}
