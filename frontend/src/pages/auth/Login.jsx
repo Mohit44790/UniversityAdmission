3872b074af2866bdfe41a62bebec255fcf7b28c5
mohit <mohit44790@gmail.com>
2026-01-14 21:54:19 +0530

    email

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -29,9 +29,17 @@ const Login = () => {
       <div className='relative z-10 h-full flex flex-col justify-center px-10'>
         <h2 className='text-3xl font-bold text-center mb-8 tracking-wide'>Admission Portal</h2>
         <form onSubmit={handleSubmit} className='space-y-7'>
-          <div className='relative group'>
-            <i className='fas fa-user absolute top-3 left-1 transition' />
-          </div>
+          <div className="relative group">
+              <i className="fas fa-user absolute top-3 left-1  transition" />
+              <input
+                type="email"
+                name="email"
+                placeholder="User name / Email"
+                onChange={handleChange}
+                className="w-full bg-transparent border-b-2 border-white/40 pl-8 py-2 font-semibold   focus:outline-none focus:border-white transition"
+                required
+              />
+            </div>
 
         </form>
       </div>
