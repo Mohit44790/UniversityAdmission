c9a46d70859364c18f65f4680543efc08bd420bd
mohit <mohit44790@gmail.com>
2026-01-15 22:38:01 +0530

    profile routes

--- frontend/src/redux/slices/profileSlice.js
+++ frontend/src/redux/slices/profileSlice.js
@@ -1,10 +1,23 @@
-// import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
-// import api from "../config/api";
-// import { setSessionData, getSessionData, removeSessionData } from "../../utils/helpers";
+import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../config/api";
+import { setSessionData, getSessionData, removeSessionData } from "../../utils/helpers";
+
+// 1️⃣ Save Basic Details
+export const saveBasicDetails = createAsyncThunk(
+  "profile/saveBasic",
+  async (payload, { rejectWithValue }) => {
+    try {
+      const res = await api.post("/api/student/profile/basic", payload);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);
 
 
-// const profileSlice = createSlice({
-//     name: "profile",
-// })
+const profileSlice = createSlice({
+    name: "profile",
+})
 
-// export default profileSlice.reducer;
+export default profileSlice.reducer;
