29c53fdbc1d0e440269cca97e8ce49f85b8f3d71
mohit <mohit44790@gmail.com>
2026-01-19 21:24:11 +0530

    profile fetch

--- frontend/src/pages/profileComplete/Profile.jsx
+++ frontend/src/pages/profileComplete/Profile.jsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchStudentProfile } from "../../redux/slices/profileSlice";
 
 
 
 const Profile = () => {
   const dispatch = useDispatch();
   const {profile ,loading ,error} = useSelector((state)=> state.profile);
+
+  useEffect(()=>{
+    if(!profile){
+      dispatch(fetchStudentProfile());
+    }
+  },[dispatch,profile]);
+
+   if (loading) {
+    return (
+      <div className="p-6 text-center text-gray-600">
+        Loading profile...
+      </div>
+    );
+  }
 
 
   return (
