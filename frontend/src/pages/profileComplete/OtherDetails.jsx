229743d0aec607ef5943539210c401380bbaea0f
mohit <mohit44790@gmail.com>
2026-01-21 17:20:10 +0530

    other formdata

--- frontend/src/pages/profileComplete/OtherDetails.jsx
+++ frontend/src/pages/profileComplete/OtherDetails.jsx
@@ -4,7 +4,14 @@ const OtherDetails = () => {
   const [loading, setLoading] = useState(false);
 
   const [formData, setFormData] = useState({
-    
+    pwbd:false,
+    kashmiriMigrant:false,
+    pmss:false,
+    defenceWard:false,
+    hasDefenceCertificate:false,
+    medicalCondition:false,
+    abcId:"",
+    universityEmployeeId:"",
   })
   return (
     <div>OtherDetails</div>
