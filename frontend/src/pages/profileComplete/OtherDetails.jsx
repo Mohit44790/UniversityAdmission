2a42d39edb329a0ecaf27c23a403e0ac595aaebc
mohit <mohit44790@gmail.com>
2026-01-21 20:06:40 +0530

    other handlechange redux h

--- frontend/src/pages/profileComplete/OtherDetails.jsx
+++ frontend/src/pages/profileComplete/OtherDetails.jsx
@@ -42,7 +42,12 @@ const OtherDetails = () => {
     }
   };
   return (
-    <div>OtherDetails</div>
+    <div className="max-w-4xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-6 text-[#4C489D]">
+        Other Details
+      </h1>
+
+    </div>
   )
 }
 
