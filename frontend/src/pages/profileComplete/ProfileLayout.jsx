c93a2c8c5c9c7102b758ef319f61c5e0be225dc9
mohit <mohit44790@gmail.com>
2026-01-22 20:45:27 +0530

    layout

--- frontend/src/pages/profileComplete/ProfileLayout.jsx
+++ frontend/src/pages/profileComplete/ProfileLayout.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+// pages/profileComplete/ProfileLayout.jsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+import ProfileStepper from "./ProfileStepper";
 
 const ProfileLayout = () => {
   return (
-    <div>ProfileLayout</div>
-  )
-}
+    <div className="max-w-6xl mx-auto p-6">
+      <ProfileStepper />
+      <div className="bg-white p-6 rounded shadow mt-6">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
-export default ProfileLayout
+export default ProfileLayout;
