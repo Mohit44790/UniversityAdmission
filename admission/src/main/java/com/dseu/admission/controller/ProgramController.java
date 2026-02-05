8dd23987152c940fa4e2f95604621818d310ad0d
mohit <mohit44790@gmail.com>
2026-02-05 14:14:36 +0530

    program selected

--- admission/src/main/java/com/dseu/admission/controller/ProgramController.java
+++ admission/src/main/java/com/dseu/admission/controller/ProgramController.java
@@ -3,27 +3,28 @@ package com.dseu.admission.controller;
 import com.dseu.admission.entity.Program;
 import com.dseu.admission.service.ProgramService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/admin/programs")
+
 @RequiredArgsConstructor
 public class ProgramController {
 
     private final ProgramService service;
 
-    @PostMapping
+    @PostMapping("/api/admin/programs")
     public Program create(@RequestBody Map<String, Object> req) {
         return service.create(
                 req.get("programName").toString(),
                 Long.valueOf(req.get("programLevelId").toString())
         );
     }
-
-    @GetMapping
+    @PreAuthorize("hasAnyRole('STUDENT','ADMIN')")
+    @GetMapping("/api/student/programs")
     public List<Program> byLevel(@RequestParam String level) {
         return service.getByLevel(level);
     }
